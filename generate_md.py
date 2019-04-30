e149fd3f784070816c52c58818c00a7f2aa26bdc
Syed Paymaan Raza <syedpaymaanraza@Syeds-MacBook-Air.local>
2019-04-29 22:52:23 -0700

    add sorting to generate_md.py

--- generate_md.py
+++ generate_md.py
@@ -9,13 +9,17 @@ def populate_rev_index(map, rev_index):
                     rev_index[hashtag] = list()
                 rev_index[hashtag].append(rec)
 
+def sort_rev_index_map(rev_index):
+    for hashtag in rev_index:
+        rev_index[hashtag].sort(key=lambda x: x["title"].lower())
+
 def dump_rec(rev_index, out_file):
     f = open(out_file, 'w')
-    for hashtag in rev_index:
+    for hashtag in sorted(rev_index):
         f.write("## " + hashtag)
         f.write('\n')
         for rec in rev_index[hashtag]:
-            f.write(rec["title"])
+            f.write(rec["title"].lower())
             f.write(" - ")
             f.write("[link](")
             f.write(rec["url"])
@@ -27,6 +31,7 @@ def dump_rec(rev_index, out_file):
 def gen_md(map):
     rev_index = dict()
     populate_rev_index(map, rev_index)
+    sort_rev_index_map(rev_index)
     dump_rec(rev_index, "output.md")
 
 # Main
